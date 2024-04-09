f8e4d0fa9b76e049290f895bb4195ebaffdd647c
Lazizbek <oddiybolakay05@gmail.com>
2024-04-09 05:22:10 +0500

    branch to main

--- js/main.js
+++ js/main.js
@@ -2,3 +2,13 @@ setInterval(
   () => document.querySelector(".userImg").classList.toggle("has-story"),
   5000
 );
+
+const loading = document.getElementById("loading");
+
+const loadingDuration = 3000; // 2s
+
+setTimeout(() => {
+  loading.classList.add("loading-none");
+}, loadingDuration);
+
+  
